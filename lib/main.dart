c88859356933f0fb15047e9eb6aeaeb495e5af7c
Tumi <oluwatumininuojo25@gmail.com>
2021-03-26 01:56:22 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -78,6 +78,29 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   void _pushedSaved() {
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+            builder: (BuildContext context) {
+              final tiles = _saved.map(
+                      (WordPair pair) {
+                    return ListTile(
+                      title: Text(pair.asPascalCase, style: _biggerFont,),
+                    );
+                  }
+              );
+              final divided = ListTile.divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList();
 
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Saved Suggestions'),
+                ),
+                body: ListView(children: divided,),
+              );
+            }
+        )
+    );
   }
 }
